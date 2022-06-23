7c2589e85cdaf628d14f7a8695968c3368ea9ebe
andreicos1 <andreicos1@yahoo.com>
2022-06-23 21:16:33 +0300

    fix email confirmation token request

--- app/pages/confirm-email/[token].tsx
+++ app/pages/confirm-email/[token].tsx
@@ -1,34 +1,52 @@
-import { Box } from "@chakra-ui/react";
-import { GetServerSideProps } from "next";
+import { Box, useToast } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { SecondaryNavigation } from "../../components/Navbar/SecondaryNavigation/SecondaryNavigation";
-import { BASE_URL } from "../../constants";
+import { BASE_URL, TOAST_ERROR } from "../../constants";
+
 import styles from "./ConfirmEmail.module.scss";
 
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  try {
-    const response = await fetch(`${BASE_URL}/auth/confirm`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token: query.token }),
-    });
-    if (response.status > 299) return { props: { confirmed: false } };
-    return { props: { confirmed: true } };
-  } catch (error) {
-    return { props: { confirmed: false } };
-  }
-};
+const ConfirmEmail = () => {
+  const router = useRouter();
+  const toast = useToast(TOAST_ERROR);
+  const confirm = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/auth/confirm`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: router.query.token }),
+      });
+      if (response.status > 299) return toast({ title: "Something went wrong. Please try again" });
+      toast({ title: "Email confirmed successfully", status: "success" });
+    } catch (error) {
+      return toast({ title: "Something went wrong. Please try again" });
+    }
+  };
 
-const ConfirmEmail = ({ confirmed }: { confirmed: boolean }) => {
+  useEffect(() => {
+    if (router.query.token) confirm();
+  }, [router.query.token]);
+
   return (
     <Box className={styles.root}>
       <Box className={styles.confirmEmail}>
         <SecondaryNavigation />
         <p className={styles.message}>
-          {confirmed
-            ? "Your account has been confirmed!"
-            : "An error has occurred. Please try again."}
+          For any questions / suggestions, leave me a message on
+          <a
+            style={{ color: "#7fa1dc" }}
+            href="https://www.linkedin.com/in/andrei-cosmin-radu-2b0886150/"
+          >
+            {" "}
+            LinkedIn
+          </a>{" "}
+          or{" "}
+          <a style={{ color: "#7fa1dc" }} href="https://twitter.com/AndreiR02443803">
+            {" "}
+            Twitter
+          </a>
         </p>
       </Box>
     </Box>
