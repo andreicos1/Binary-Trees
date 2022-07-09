dce469f38d56a516b0d231ccbff83e59feb506b8
andreicos1 <andreicos1@yahoo.com>
2022-07-09 16:42:38 +0300

    change ports & lock mobile landscape

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,13 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import Head from "next/head";
+import { useEffect } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar List/Navbar";
 import Reactions from "../components/Reactions/ReactionsBar/Reactions";
 import ToolkitCanvasBox from "../components/ToolkitCanvasBox/ToolkitCanvasBox";
 import styles from "../styles/Home.module.scss";
 
-
 const Home = () => {
+  useEffect(() => {
+    if (screen.orientation) screen.orientation.lock("landscape");
+  }, []);
+
   return (
     <Box>
       <Head>
@@ -22,7 +26,7 @@ const Home = () => {
         <Navbar />
         <Box className={styles.content}>
           <ToolkitCanvasBox />
-          <Reactions/>
+          <Reactions />
         </Box>
         <Footer />
       </Box>
